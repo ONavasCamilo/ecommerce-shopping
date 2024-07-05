7e70af1d365067880c9700c3f2644af429c9db4e
camilo Navas <navasochoa@hotmail.com>
2024-07-04 22:18:44 -0500

    grid icons navbar

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,17 +1,17 @@
 import iconMenu from "/iconMenu.svg";
 import style from "./Header.module.css";
+import iconLogo from "/iconLogo.svg";
+import iconAccount from "/iconAccount.svg";
+import iconCart from "/iconCart.svg";
 
 const Header = () => {
   return (
     <div className={style.div__navbar}>
-      <img src={iconMenu} className={style.icon__menu}/>
-      <h1>SHOPPYSTYLE</h1>
-      <h2>a</h2>
-      <h2 className={style.icon__account}>cc</h2>
-      <h2 className={style.icon__cart}>r</h2>
-      {/* <img src={iconLogo} /> */}
-      {/* <img src={iconAccount} className={style.icon__account}/> */}
-      {/* <img src={iconCart}  className={style.icon__cart}/> */}
+        <img src={iconMenu} className={style.icon__menu} />
+        <img src={iconLogo} className={style.icon__logo} />
+        <img src={iconAccount} className={style.icon__account} />
+        {/* <span>Cuenta</span> */}
+        <img src={iconCart} className={style.icon__cart} />
     </div>
   );
 };
